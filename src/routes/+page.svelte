340c31b320b0ee9a510d42124db699e43b1eab26
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-02-10 15:24:04 -0300

    Features in a box model

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div>
+	<div class="box">
+		<img src="bolt.svg" alt="" />
+		<p>Acelere seus estudos com IA</p>
+	</div>
+</div>
+
+<style>
+	.box {
+		height: 100px;
+		width: 100px;
+		background-color: rgb(51, 51, 51);
+		border-radius: 7px;
+		padding: 5px;
+		/* border: 3px solid; */
+		/* border-color: rgba(168, 175, 168, 0.5); */
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		font-size: 0.6em;
+		text-align: center;
+		color: whitesmoke;
+	}
+	.box img {
+		filter: invert(94%) sepia(6%) saturate(0%) hue-rotate(113deg) brightness(105%) contrast(108%);
+	}
+</style>
