a736119520f1c96140695e28db04b6c582bf5248
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-02-10 15:27:38 -0300

    Feature box as a component

---
+++ src/lib/feature_box.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let path = '';
+	export let feature = '';
+</script>
+
+<div class="box">
+	<img src={path} alt="" />
+	<p>{feature}</p>
+</div>
+
+<style>
+	.box {
+		height: 100px;
+		width: 100px;
+		background-color: rgb(51, 51, 51);
+		border-radius: 7px;
+		padding: 5px;
+		/* border: 3px solid; */
+		/* border-color: rgba(168, 175, 168, 0.5); */
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		font-size: 0.6em;
+		text-align: center;
+		color: whitesmoke;
+	}
+	.box img {
+		filter: invert(94%) sepia(6%) saturate(0%) hue-rotate(113deg) brightness(105%) contrast(108%);
+	}
+</style>
