b4569b0c2416eaa088d5fdb2a68d839268261fe3
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-02-10 13:48:25 -0300

    Base gradient navbar

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,47 @@
+<div class="nav-div">
+	<div class="nav-title">
+		<h2>Conquista Concursos</h2>
+	</div>
+	<ul>
+		<li><a href="/">Começar</a></li>
+		<li><a href="/">Cadernos</a></li>
+		<li><a href="/sobre">Sobre</a></li>
+	</ul>
+</div>
+
+<style>
+	.nav-div {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 15px 3%;
+		background: linear-gradient(to right, #2be4ad, #eaecc6);
+		height: 30px;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	ul li {
+		display: inline-block;
+	}
+
+	ul li a {
+		text-decoration: none;
+		color: rgb(46, 46, 46);
+		padding: 15px 25px;
+		border-radius: 7px;
+		transition: 0.3s;
+		box-shadow: 0 0 0 0rem black;
+		cursor: pointer;
+	}
+
+	ul li a:hover {
+		border-bottom: 20px;
+		box-shadow: 0 0 0 0.1rem black;
+		font-size: 50;
+		color: white;
+		background-color: #a5c599;
+	}
+</style>
