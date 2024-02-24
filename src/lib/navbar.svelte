2569361f731bf195d94b7943b394412e1dd1cad1
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-02-24 18:38:36 -0300

    New navbar with hamburger menu

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,23 +1,26 @@
-<div class="nav-div">
+<nav class="top-nav">
 	<div class="nav-title">
-		<img src="award_star.svg" alt="" />
+		<img src="award_star.svg" alt="" width="10%" height="10%" />
 		<a href="/">Conquista Concursos</a>
 	</div>
-	<ul>
+	<input id="menu-toggle" type="checkbox" />
+	<label class="menu-button-container" for="menu-toggle">
+		<div class="menu-button"></div>
+	</label>
+	<ul class="nav-menu">
 		<li><a href="/simulados" class="simulado">Simulados do CNU</a></li>
 		<li><a href="/sobre">Sobre</a></li>
 		<li><a href="/">Login</a></li>
 	</ul>
-</div>
+</nav>
 
 <style>
-	.nav-div {
+	.top-nav {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding: 15px 3%;
 		background: rgb(241, 241, 241);
-		height: 30px;
+		height: 8vh;
 	}
 
 	.nav-title a {
@@ -25,6 +28,7 @@
 		color: black;
 		font-size: large;
 		font-weight: bold;
+		font-size: 2vh;
 	}
 
 	ul {
@@ -38,15 +42,15 @@
 	ul li a {
 		text-decoration: none;
 		color: black;
-		padding: 15px 25px;
+		padding: 1vh 2.5vw;
 		border-radius: 20px;
 		transition: 0.3s;
 		cursor: pointer;
+		font-size: 1.8vh;
 	}
 
 	ul li a:hover {
 		box-shadow: 0 0 0 0.1rem black;
-		font-size: 50;
 		color: black;
 		background-color: #a5c599;
 	}
@@ -60,5 +64,119 @@
 		color: black;
 		background-color: steelblue;
 		/* box-shadow: 0 0 0 0.03 black; */
+	}
+
+	.top-nav {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		background: rgb(241, 241, 241);
+		/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+		padding: 0 3vw 0 3vw;
+	}
+
+	.nav-menu {
+		display: flex;
+		flex-direction: row;
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	.menu-button-container {
+		display: none;
+		max-height: 10px;
+		width: 30px;
+		cursor: pointer;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	#menu-toggle {
+		display: none;
+	}
+
+	.menu-button,
+	.menu-button::before,
+	.menu-button::after {
+		display: block;
+		background-color: black;
+		position: absolute;
+		height: min(4px, 1vh);
+		width: 25px;
+		transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+		border-radius: 2px;
+	}
+
+	.menu-button::before {
+		content: '';
+		margin-top: max(-8px, -2vh);
+	}
+
+	.menu-button::after {
+		content: '';
+		margin-top: min(8px, 2vh);
+	}
+
+	#menu-toggle:checked + .menu-button-container .menu-button::before {
+		margin-top: 0px;
+		transform: rotate(405deg);
+	}
+
+	#menu-toggle:checked + .menu-button-container .menu-button {
+		background: rgba(255, 255, 255, 0);
+	}
+
+	#menu-toggle:checked + .menu-button-container .menu-button::after {
+		margin-top: 0px;
+		transform: rotate(-405deg);
+	}
+
+	@media (max-width: 700px) {
+		.menu-button-container {
+			display: flex;
+		}
+		.nav-menu {
+			position: absolute;
+			top: 0;
+			margin-top: 8vh;
+			left: 0;
+			flex-direction: column;
+			width: 100%;
+			justify-content: center;
+			align-items: center;
+		}
+		#menu-toggle ~ .nav-menu li {
+			height: 0;
+			margin: 0;
+			padding: 0;
+			border: 0;
+			transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+		}
+		#menu-toggle:checked ~ .nav-menu li {
+			border: 1px solid #333;
+			height: 5vh;
+			padding: 0.5vh;
+			transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+		}
+		.nav-menu > li {
+			display: flex;
+			justify-content: right;
+			align-items: center;
+			padding-right: 5vw;
+			width: 100%;
+			background-color: #363636;
+			overflow: hidden;
+		}
+
+		.nav-menu > li > a {
+			margin-right: 3vw;
+			color: white;
+		}
+		.nav-menu > li:not(:last-child) {
+			border-bottom: 1px solid #444;
+		}
 	}
 </style>
