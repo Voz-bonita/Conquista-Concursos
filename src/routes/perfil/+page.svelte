e9385b933aa38092b98be204dab5dbaf89421231
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-03-10 14:29:14 -0300

    Almost blank profile and about pages

---
+++ src/routes/perfil/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { authStore } from '$lib/stores/authStore';
+
+	let name;
+	authStore.subscribe((curr) => {
+		name = curr?.currentUser?.email;
+	});
+</script>
+
+{#if $authStore.currentUser}
+	{name}
+{:else}
+	<div>Carregando...</div>
+{/if}
+
+<style>
+</style>
---
+++ src/routes/sobre/+page.svelte
@@ -0,0 +1,4 @@
+<div class="temp">
+	<h2>Sobre nós</h2>
+	<p>Isso é um teste</p>
+</div>
