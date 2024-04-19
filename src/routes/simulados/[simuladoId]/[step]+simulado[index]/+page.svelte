4058d004ba20726c0d5bd24ee87067c45575b526
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-04-19 07:29:45 -0300

    Nex question button

--- src/routes/simulados/[simuladoId]/[step]+simulado[index]/+page.svelte
+++ src/routes/simulados/[simuladoId]/[step]+simulado[index]/+page.svelte
@@ -8,6 +8,9 @@
 
 <div class="page-body green-theme">
     <ObjectiveQuestion question={questions[questionIndex]} question_index={questionIndex}/>
+    <div class="directional-btns-div">
+        <button class="next-button clickable" on:click|preventDefault={() => {questionIndex++}}>Próxima</button>
+    </div>
 </div>
 
 
@@ -16,5 +19,21 @@
         min-height: calc(100vh - 8vh - 8vh);
         display: flex;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+    .next-button {
+        font-size: 1.2vw;
+        background: steelblue;
+        color: white;
+        border-radius: 20px;
+        border: 1px solid black;
+        padding: 10px 40px 10px 40px;
+        float: right;
+        margin-bottom: 1vh;
+    }
+    .directional-btns-div {
+        margin-top: 1vh;
+        width: 80%;
     }
 </style>
