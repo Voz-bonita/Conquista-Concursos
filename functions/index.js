12c2dc36c35f32ae1ae4b447712d57ff5b6e0d5f
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-04-27 13:21:51 -0300

    File index.js must contain every cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,54 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onCall, onRequest } = require('firebase-functions/v2/https');
+const { config } = require('firebase-functions');
+const { initializeApp } = require('firebase-admin/app');
+const { getDatabase } = require('firebase-admin/database');
+const { doc, setDoc } = require('firebase/firestore');
 
-const { onRequest } = require('firebase-functions/v2/https');
-const logger = require('firebase-functions/logger');
+const admin_app = initializeApp();
+const db = getDatabase(admin_app);
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.createStripeCheckout = onCall(async (data, context) => {
+	const stripe = require('stripe')(config().stripe.secret);
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ['card'],
+		mode: 'payment',
+		success_url: 'http://localhost:5173/',
+		cancel_url: 'http://localhost:5173/sobre',
+		line_items: [
+			{
+				quantity: 1,
+				price_data: {
+					currency: 'brl',
+					unit_amount: 20 * 100,
+					product_data: {
+						name: 'Simulado 1'
+					}
+				}
+			}
+		]
+	});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+	return { id: session.id };
+});
+
+exports.stripeWebhook = onRequest(async (request, response) => {
+	const stripe = require('stripe')(config().stripe.secret);
+	const webhook = require('stripe')(config().stripe.webhook_secret);
+
+	let event;
+	try {
+		event = stripe.webhook.constructEvent(req.body, req.headers['stripe-signature'], webhook);
+	} catch (error) {
+		console.error('O Webhook falhou');
+		return response.sendStatus(403);
+	}
+
+	const dataObject = event.data.object;
+	const dataAsDoc = {
+		checkoutSessionId: dataObject.id,
+		paymentStatus: dataObject.payment_status,
+		amountTotal: dataObject.amount_total
+	};
+	const docRef = doc(db, 'users/teste');
+	await setDoc(docRef);
+});
