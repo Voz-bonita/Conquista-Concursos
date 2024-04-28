e67c00a5cd47b60ce3f01a164063361688d696b1
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-04-28 14:45:37 -0300

    Prefill user email on Stripe

--- functions/index.js
+++ functions/index.js
@@ -6,15 +6,18 @@ const { setGlobalOptions } = require('firebase-functions/v2');
 const { defineSecret } = require('firebase-functions/params');
 
 const STRIPE_SECRET = defineSecret('STRIPE_SECRET');
+const STRIPE_WEBHOOK = defineSecret('STRIPE_WEBHOOK');
 
-setGlobalOptions({ region: 'southamerica-east1' });
+setGlobalOptions({ region: 'us-central1' });
 
 const admin_app = initializeApp();
 const db = getFirestore(admin_app);
 
 exports.createStripeCheckout = onCall({ secrets: [STRIPE_SECRET] }, async ({ data, auth }) => {
+	const contestId = data.shoppingCart;
+
 	let metadata;
-	const docPromisse = db.collection('contests_questions').doc(data.shoppingCart).get();
+	const docPromisse = db.collection('contests_questions').doc(contestId).get();
 	await docPromisse
 		.then((snapshot) => {
 			metadata = snapshot.data().metadata;
@@ -25,11 +28,14 @@ exports.createStripeCheckout = onCall({ secrets: [STRIPE_SECRET] }, async ({ dat
 
 	const stripe_module = require('stripe');
 	const stripe = stripe_module.Stripe(STRIPE_SECRET.value());
+
 	const session = await stripe.checkout.sessions.create({
+		customer_email: auth.token.email,
 		payment_method_types: ['card'],
 		mode: 'payment',
 		success_url: 'http://localhost:5173/',
 		cancel_url: 'http://localhost:5173/sobre',
+		metadata: { id: contestId, customer_uid: auth.token.uid },
 		line_items: [
 			{
 				quantity: 1,
@@ -48,24 +54,33 @@ exports.createStripeCheckout = onCall({ secrets: [STRIPE_SECRET] }, async ({ dat
 	return { id: session.id };
 });
 
-exports.stripeWebhook = onRequest(async (request, response) => {
-	const stripe = require('stripe')(config().stripe.secret);
-	const webhook = require('stripe')(config().stripe.webhook_secret);
+exports.stripeWebhook = onRequest(
+	{ secrets: [STRIPE_SECRET, STRIPE_WEBHOOK] },
+	async (request, response) => {
+		const stripe_module = require('stripe');
+		const stripe = stripe_module.Stripe(STRIPE_SECRET.value());
+		let event;
+		try {
+			event = stripe.webhooks.constructEvent(
+				request.rawBody,
+				request.headers['stripe-signature'],
+				'whsec_cf090b333eedcae04d46f0ec95bc0c30e28e31a15127ac8d67fd24b4bdd0f7d6'
+			);
+		} catch (error) {
+			console.error('O Webhook falhou');
+			return response.sendStatus(403);
+		}
 
-	let event;
-	try {
-		event = stripe.webhook.constructEvent(req.body, req.headers['stripe-signature'], webhook);
-	} catch (error) {
-		console.error('O Webhook falhou');
-		return response.sendStatus(403);
+		const dataObject = event.data.object;
+		const eventType = event.type;
+		switch (eventType) {
+			case 'checkout.session.completed':
+				const session = await stripe.checkout.sessions.retrieve(dataObject.id);
+				const contestId = session.metadata.id;
+				const uid = session.metadata.customer_uid;
+
+				break;
+		}
+		response.sendStatus(200);
 	}
-
-	const dataObject = event.data.object;
-	const dataAsDoc = {
-		checkoutSessionId: dataObject.id,
-		paymentStatus: dataObject.payment_status,
-		amountTotal: dataObject.amount_total
-	};
-	const docRef = doc(db, 'users/teste');
-	await setDoc(docRef);
-});
+);
