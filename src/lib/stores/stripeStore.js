40bc1f78f2cf2cae56b6d9ef28536e539a51957f
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-04-28 20:14:31 -0300

    Workaround await on top

--- src/lib/stores/stripeStore.js
+++ src/lib/stores/stripeStore.js
@@ -3,9 +3,15 @@ import { loadStripe } from '@stripe/stripe-js';
 
 const stripe_public =
 	'pk_live_51P9vWcP079yRGsvdlS8iRz7C5lBHcLNbN7Ut1mq08raYsYDQhb0bImdDawcdaog0pFlx7ZnNV875rLlT19GdYR8W00heEIN7Z2';
-const stripe = await loadStripe(stripe_public);
+
+var stripe = null;
+async function initStripe() {
+	stripe = await loadStripe(stripe_public);
+}
+initStripe();
 
 const functions = getFunctions();
+
 const createStripeCheckout = httpsCallable(functions, 'createStripeCheckout');
 
 export const stripeHandler = {
