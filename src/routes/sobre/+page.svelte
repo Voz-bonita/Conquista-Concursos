abf3d53fa8df84a90c35b0ee53233ff0c4a4e61a
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-04-28 22:13:11 -0300

    About page and style

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -1,4 +1,26 @@
-<div class="temp">
-	<h2>Sobre nós</h2>
-	<p>Isso é um teste</p>
+<div class="text-only-wrapper">
+	<h1>Sobre o Conquista Concursos</h1>
+	<p>
+		Olá, eu me chamo José Vítor, sou dono e desenvolvedor do conquista-concursos.com, trata-se de uma nova plataforma de estudos para concursos. Ano passado, eu prestei o concurso do Banco do Brasil para o cargo de Agente de Tecnologia, e embora eu tenha sido aprovado, eu sentia que os simulados que eu fazia durante a minha preparação não refletiam realmente a forma como a banca cobrava os conteúdos. 
+	</p>
+	<p>
+		Esse incômodo me trouxe uma ideia 💡:  seria possível fazer simulados com questões inéditas que copiem a estilometria da banca que vai elaborar a prova, utilizando Inteligência Artifical (IA)? 
+	</p>
+	<p>
+		Tendo em vista a amplitude do Concurso Nacional Unificado (CNU), resolvi tirar este projeto da gaveta e finalmente dar asas a ele. Como programador, eu sabia do desafio que seria essa jornada, porém nada poderia me preparar para as intempéries que estavam por vir. 
+	</p>
+	<p>
+		O resultado: nos 45 do segundo tempo o projeto nasceu e eu espero que ele contribua para a lapidação do seu estudo e para a conquista da sua aprovação. 
+	</p>
+	<br/>
+	<p>
+		<i>Per aspera ad astra</i> (alcançar o triunfo (a imortalidade) por feitos notáveis))
+	</p>
+	<p>Conquista Concursos</p>
 </div>
+
+<style>
+	p {
+		font-size: 1.3rem;
+	}
+</style>
