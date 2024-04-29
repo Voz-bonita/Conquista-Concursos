47d557688360da26d05a53c0c0998e673389b079
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-04-28 22:05:33 -0300

    Phone support for pix payment

--- src/lib/components/contest_step_picking.svelte
+++ src/lib/components/contest_step_picking.svelte
@@ -1,4 +1,6 @@
 <script>
+	import pix from '$lib/assets/pix.svg';
+
 	import ContestRadio from '$lib/components/contest_radio.svelte';
 	import ContestIndexRadio from '$lib/components/contest_index_radio.svelte';
 	import { enhance } from '$app/forms';
@@ -49,6 +51,19 @@
 		<ContestIndexRadio {contestReplicates} bind:userSelected={version} />
 	</div>
 
+	<div class="pix-warning">
+		<img src={pix} alt="" class="pix-icon">
+		<p>Deseja pagar por Pix? Entre em contato por WhatsApp com <a
+			href="https://api.whatsapp.com/send/?phone=5561982851618&text=Ol%C3%A1!%20Eu%20gostaria%20de%20comprar%20um%20simulado%20do%20Conquista%20Concursos.%20Pode%20me%20ajudar?&type=phone_number&app_absent=0"
+			target="_blank"
+			rel="noopener noreferrer">(61) 9 8285-1618</a
+		></p>
+	</div>
+	<div class="support-warning">
+		<p>Suporte por WhatsApp somente das 19h às 22h (Segunda a Sexta)</p>
+		<p>Suporte por WhatsApp somente das 9h às 23h (Sábados, Domingos e Feriados)</p>
+	</div>
+	
 	<div class="directional-btns-div">
 		<button class="back-button directional-btn clickable" on:click|preventDefault={backToListing}
 			>Voltar</button
@@ -64,6 +79,7 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		max-width: 99vw;
 	}
 	.contest-frame {
 		background: rgb(241, 241, 241);
@@ -121,5 +137,43 @@
 		width: 80%;
 		display: flex;
 		justify-content: right;
+	}
+	.pix-warning {
+		background: whitesmoke;
+		border-radius: 20px;
+		border: 1px solid black;
+		font-size: 0.8vw;
+		padding: 0px 10px 0px 10px;
+		margin-top: 1vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		max-width: 70%;
+		flex-direction: row;
+	}
+
+	.support-warning {
+		background: whitesmoke;
+		border-radius: 20px;
+		border: 1px solid black;
+		font-size: 0.8vw;
+		padding: 0px 10px 0px 10px;
+		margin-top: 1vh;
+		max-width: 50%;
+		
+	}
+
+	.pix-icon {
+		filter: invert(66%) sepia(53%) saturate(385%) hue-rotate(125deg) brightness(86%) contrast(91%);
+		margin-right: 10px;
+		height: 70%;
+		padding: 10px;
+		width: 5%;
+	}
+
+	@media screen and (orientation: portrait), screen and (max-width: 700px) {
+		.pix-warning {
+			font-size: 2.2vw;
+		}
 	}
 </style>
