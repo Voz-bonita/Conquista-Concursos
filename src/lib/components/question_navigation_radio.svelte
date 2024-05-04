9988eab43be886b98f1df2c1f5d42ad647ebf90b
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-05-04 10:14:51 -0300

    Responsive content with "centered" text

--- src/lib/components/question_navigation_radio.svelte
+++ src/lib/components/question_navigation_radio.svelte
@@ -6,14 +6,16 @@
 <div class="navigation" role="radiogroup">
 	{#each questions as _, i (i)}
 		<label for={`Q${i}`} class="indexing-btn clickable">
-			<input
-				id={`Q${i}`}
-				style="display: none;"
-				type="radio"
-				value={i}
-				bind:group={currentQuestionIndex}
-			/>
-			<label for={`Q${i}`} class="index">{i + 1}</label>
+			<div class="index-div">
+				<input
+					id={`Q${i}`}
+					style="display: none;"
+					type="radio"
+					value={i}
+					bind:group={currentQuestionIndex}
+				/>
+				<label for={`Q${i}`} class="index">{i + 1}</label>
+			</div>
 		</label>
 	{/each}
 </div>
@@ -23,27 +25,32 @@
 		max-width: 80%;
 		margin-top: 2vh;
 	}
+	.index-div {
+		display: flex;
+		justify-content: center;
+	}
 	.indexing-btn {
 		background-color: whitesmoke;
 		color: black;
 		margin: 3px;
-		text-align: center;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		padding: 3px;
+		width: 0.8em;
+		padding: 0.3em 0.5em;
 		border-radius: 7px;
 		border: 1px solid black;
-		min-width: 1.6vw;
-		max-height: 3vh;
 		display: inline-block;
 	}
-	label:has(> input[type='radio']:checked) {
+	label:has(div > input[type='radio']:checked) {
 		background: rgb(26, 25, 25);
 		color: white;
 	}
 	.index {
 		cursor: pointer;
 		font-size: 0.8vw;
+	}
+
+	@media screen and (orientation: portrait), screen and (max-width: 700px) {
+		.index {
+			font-size: 1.8vw;
+		}
 	}
 </style>
