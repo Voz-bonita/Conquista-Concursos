3859718669b38f6eb5d4ffa66826ed41051c8538
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-05-25 09:23:44 -0300

    Version 1 for all contests

--- src/routes/simulados/+page.server.js
+++ src/routes/simulados/+page.server.js
@@ -2,7 +2,8 @@ export const actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
 		const step = formData.get('step');
-		const version = formData.get('version');
+		// const version = formData.get('version');
+		const version = 1;
 		const document = `${step}_${version - 1}`;
 
 		return { document };
