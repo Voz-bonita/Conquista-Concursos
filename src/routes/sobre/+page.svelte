1f33c18cb271fad63e23cfbb6b55d885988a37dc
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-05-26 10:54:06 -0300

    Third person speech

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -1,16 +1,16 @@
 <div class="text-only-wrapper">
 	<h1>Sobre o Conquista Concursos</h1>
 	<p>
-		Olá, eu me chamo José Vítor, sou dono e desenvolvedor do conquista-concursos.com, trata-se de uma nova plataforma de estudos para concursos. Ano passado, eu prestei o concurso do Banco do Brasil para o cargo de Agente de Tecnologia, e embora eu tenha sido aprovado, eu sentia que os simulados que eu fazia durante a minha preparação não refletiam realmente a forma como a banca cobrava os conteúdos. 
+		Olá, concurseiro! O conquista-concursos.com se trata de uma nova plataforma de estudos para concursos. Nós, fundadores do projeto, também prestamos concursos públicos no passado, mas mesmo depois de conquistar a aprovação, sentiamos que os simulados que feitos durante a preparação não refletiam realmente a forma como a banca cobrava os conteúdos. 
 	</p>
 	<p>
-		Esse incômodo me trouxe uma ideia 💡:  seria possível fazer simulados com questões inéditas que copiem a estilometria da banca que vai elaborar a prova, utilizando Inteligência Artifical (IA)? 
+		Esse incômodo nos trouxe uma ideia 💡:  seria possível fazer simulados com questões inéditas que copiem a estilometria da banca que vai elaborar a prova, utilizando Inteligência Artifical (IA)? 
 	</p>
 	<p>
-		Tendo em vista a amplitude do Concurso Nacional Unificado (CNU), resolvi tirar este projeto da gaveta e finalmente dar asas a ele. Como programador, eu sabia do desafio que seria essa jornada, porém nada poderia me preparar para as intempéries que estavam por vir. 
+		Tendo em vista a amplitude do Concurso Nacional Unificado (CNU), resolvemos tirar este projeto da gaveta e finalmente dar asas a ele. Como programadores, nos sabiamos do desafio que seria essa jornada, porém nada poderia nos preparar para as intempéries que estavam por vir. 
 	</p>
 	<p>
-		O resultado: nos 45 do segundo tempo o projeto nasceu e eu espero que ele contribua para a lapidação do seu estudo e para a conquista da sua aprovação. 
+		O resultado: nos 45 do segundo tempo o projeto nasceu e esperamos que ele contribua para a lapidação do seu estudo e para a conquista da sua aprovação. 
 	</p>
 	<br/>
 	<p>
