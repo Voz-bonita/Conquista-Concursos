4493b683c9a4e84d0097a02b8b313a9ea7591a66
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-06-01 11:55:08 -0300

    Component to show focused content

---
+++ src/lib/components/content_div.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<div class="question-frame"><slot></slot></div>
+
+<style>
+	.question-frame {
+		display: flex;
+		flex-direction: column;
+		color: black;
+		width: 80%;
+		background-color: whitesmoke;
+		border: 1px solid black;
+		border-radius: 20px;
+		padding: 1em 1.5em;
+		margin: 0vh 0 1vh 0;
+	}
+	
+	@media screen and (orientation: portrait), screen and (max-width: 700px) {
+		
+	}
+</style>
