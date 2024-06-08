2be5fc553a5fe7ab09c785f86a75f691f7ccd549
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-06-08 17:16:01 -0300

    ActionRequired component to show error messages to user

---
+++ src/lib/components/action_required.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<div class="action-container">
+    <span class="action-symbol">&#x1F7AC;</span>
+    <p class="action-text"><slot></slot></p>
+</div>
+
+<style>
+    .action-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        background-color: rgb(255, 62, 62);
+        width: 100%;
+        margin: 1rem 0rem;
+        padding: 0.1rem 0.1rem;
+    }
+
+    .action-symbol {
+        color: white;
+        font-size: 2rem;
+        padding: 0rem 1rem;
+    }
+
+    .action-text {
+        color: white;
+        font-size: 1rem;
+    }
+</style>
