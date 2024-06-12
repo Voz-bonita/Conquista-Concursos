081f3dc292b77cab3452d3ff374253a4f99d4c14
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-06-12 20:52:26 -0300

    Ubsubscribe authStore before getting out of login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,15 +1,19 @@
 <script>
 	import AuthForm from '$lib/components/auth_form.svelte';
 	import { authStore } from '$lib/stores/authStore';
-	import { goto } from '$app/navigation';
+	import { goto, beforeNavigate } from '$app/navigation';
 
 	export let data;
 
-	authStore.subscribe((current) => {
+	const unsubscribe = authStore.subscribe((current) => {
 		if (current.userLogged) {
 			goto(`/${data.origin}`);
 		}
 	});
+
+	beforeNavigate(() => {
+		unsubscribe();
+	})
 </script>
 
 <div style="border-top: 1px solid black">
