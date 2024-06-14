552aef02f3f09869595b5c3a9a21a867bf28023f
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-06-14 19:55:28 -0300

    Functions to set and unset loading

--- src/lib/stores/loadingStore.js
+++ src/lib/stores/loadingStore.js
@@ -1,2 +1,14 @@
 import { writable } from 'svelte/store';
 export const loadingStore = writable({ loading: false });
+
+export const setLoading = () => {
+	loadingStore.update(() => {
+		return { loading: true };
+	});
+};
+
+export const unsetLoading = () => {
+	loadingStore.update(() => {
+		return { loading: false };
+	});
+};
