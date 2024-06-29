2f14745214b53420f1d1bfd0d807f6b4cfcf9a84
Jose Vitor <jose.vitor.sem.c@gmail.com>
2024-06-29 09:07:36 -0300

    Get parameters for scoring the user answer

--- src/routes/gerador/+page.server.js
+++ src/routes/gerador/+page.server.js
@@ -5,6 +5,8 @@ export const actions = {
 		const data = await request.formData();
 		const subject = data.get('subject');
 		const questionType = data.get('objetiva') ?? data.get('discursiva') ?? data.get('correcao');
+		const questionBody = data.get('question-body');
+		const questionAnswer = data.get('question-answer');
 
 		if (subject.length < 4 && questionType != 'correcao') {
 			return fail(400, { questionType, subject, shortSubject: true });
@@ -16,7 +18,12 @@ export const actions = {
 			return fail(400, { subject, unselectedType: true });
 		}
 
-		const query = new URLSearchParams({ assunto: subject, tipo: questionType }).toString();
+		const query = new URLSearchParams({
+			assunto: subject,
+			tipo: questionType,
+			enunciado: questionBody,
+			resposta: questionAnswer
+		}).toString();
 		return { fetchCall: `/api?${query}`, success: true };
 	}
 };
